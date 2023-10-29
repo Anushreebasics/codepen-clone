0e61d98904112fcb3bf77f23c247ce010ffd1905
anushree <aushreebondia@gmail.com>
2023-10-29 15:34:30 +0530

    updated signup n login frontend

--- src/components/UserAuthInput.jsx
+++ src/components/UserAuthInput.jsx
@@ -1,26 +1,59 @@
 import React, { useState } from "react";
-import { FaEnvelope, FaEye } from "react-icons/fa6";
-
-const UserAuthInput = ({AbortController, placeHolder, isPass, setStateFunction, Icon}) => {
+import { FaEnvelope, FaEye, FaEyeSlash } from "react-icons/fa6";
+import { motion } from "framer-motion";
+const UserAuthInput = ({
+  label,
+  placeHolder,
+  isPass,
+  setStateFunction,
+  Icon,
+  setGetEmailValidationStatus
+}) => {
   const [value, setValue] = useState("");
+  const [showPass, setShowPass] = useState(false);
+  const [isEmailValid,setIsEmailValid]=useState(false);
+  
+  const handleTextChange=(e)=>{
+    setValue(e.target.value)
+    setStateFunction(e.target.value)
+
+    if(placeHolder==="Email"){
+        const emailRegex=/^[^\s0]+0[^\s0].[^\s0]+$/;
+        const status=emailRegex.test(value);
+        setIsEmailValid(status);
+        setGetEmailValidationStatus(status)
+    }
+  }
+
   return (
     <div className="flex flex-col items-start justify-start gap-1">
-      <label className="text-sm text-gray-300">Email</label>
+      <label className="text-sm text-gray-300">{label}</label>
       <div
-        className={`flex items-center justify-center gap-3 w-full md:w-96 rounded-md px-4 py-1 bg-gray-200`}
+        className={`flex items-center justify-center gap-3 w-full md:w-96 rounded-md px-4 py-1 bg-gray-200 ${
+            !isEmailValid && placeHolder==="Email" && value.length>0 && "border-2 border-red-500"}`}
       >
-        <FaEnvelope className="text-text555 text-2xl" />
+        <Icon className="text-text555 text-2xl" />
         <input
-          type="text"
-          placeholder="Email here.."
+          type={isPass && showPass ? "password" : "text"}
+          placeholder={placeHolder}
           className="flex-1 w-full h-full py-2 outline-none border-none bg-transparent text-text555 text-lg"
           value={value}
-          onChange={(e) => setValue(e.target.value)}
+          onChange={handleTextChange}
         />
 
-        <div className="cursor-pointer">
-          <FaEye className="text-text555 text-2xl" />
-        </div>
+        {isPass && (
+          <motion.div
+            onClick={() => setShowPass(!showPass)}
+            whileTap={{ scale: 0.9 }}
+            className="cursor-pointer"
+          >
+            {showPass ? (
+              <FaEye className="text-text555 text-2xl" />
+            ) : (
+              <FaEyeSlash className="text-text555 text-2xl" />
+            )}
+          </motion.div>
+        )}
       </div>
     </div>
   );
--- src/container/SignUp.jsx
+++ src/container/SignUp.jsx
@@ -1,12 +1,16 @@
 import React,{useState} from "react";
 import { logo } from "../assets";
 import { UserAuthInput } from "../components";
-import { FaEnvelope, FaEye } from "react-icons/fa6";
+import { FaEnvelope } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import { MdPassword } from "react-icons/md";
+import {motion} from "framer-motion" 
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [getEmailValidationStatus,setGetEmailValidationStatus]=useState(false)
+  const [isLogin,setIsLogin]=useState(false);
   return (
     <div className="w-full py-6">
       <img
@@ -17,7 +21,7 @@ const SignUp = () => {
       <div className="w-full flex flex-col items-center justify-center py-8">
         <p className="py-12 text-2xl text-primaryText">Join With Us! </p>
 
-        <div className="px-8 w-full md:w-auto py-4 rounded-xl bg-secondary shadow-md flex flex-col items-center gap-8">
+        <div className="px-8 w-full md:w-auto py-4 rounded-xl bg-secondary shadow-md flex flex-col items-center justify-center gap-8">
           {/* email */}
           <UserAuthInput
             label="Email"
@@ -26,18 +30,33 @@ const SignUp = () => {
             key="Email"
             setStateFunction={setEmail}
             Icon={FaEnvelope}
+            setGetEmailValidationStatus={setGetEmailValidationStatus}
           />
           {/* password */}
-          <UserAuthInput
-            label=""
-            placeHolder=""
-            isPass={}
-            key=""
-            setStateFunction=""
-            Icon=""
+          <UserAuthInput 
+            label="Password"
+            placeHolder="Password"
+            isPass={true}
+            key="Password"
+            setStateFunction={setPassword}
+            Icon={MdPassword}
           />
+          
           {/* alert section */}
           {/* login button */}
+          {!isLogin ? (<motion.div whileTap={{scale:0.9}} className="flex items-center justify-center w-full py-3 rounded-xl hover:bg-emerald-400 cursor-pointer bg-emerald-500">
+              <p className="text-xl text-white">Sign Up</p>
+          </motion.div>) : 
+          (<motion.div whileTap={{scale:0.9}} className="flex items-center justify-center w-full py-3 rounded-xl hover:bg-emerald-400 cursor-pointer bg-emerald-500">
+            <p className="text-xl text-white">Login</p>
+          </motion.div>)}
+
+          {!isLogin ? (
+            <p className="text-sm text-primaryText flex items-center justify-center gap-3">Already Have an account! <span onClick={()=>setIsLogin(!isLogin)} className="text-emerald-500 cursor-pointer">Login here</span></p>
+          ) : (
+            <p className="text-sm text-primaryText flex items-center justify-center gap-3">Already Have an account! <span onClick={()=>setIsLogin(!isLogin)} className="text-emerald-500 cursor-pointer">Login here</span></p>
+          )}
+
           {/* account text section */}
           {/* or section */}
           {/* sign in with google */}
